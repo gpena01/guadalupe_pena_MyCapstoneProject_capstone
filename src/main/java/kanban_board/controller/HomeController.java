6b8b6c506a550950c0ff4848c12a8a048013cd9f
Lupe <gpena09@gmail.com>
2023-07-10 19:06:05 -0700

    Bobby helped me get my loggedin page set up. So when user logs in, user will have access to creating a kanban board and a log out button will display.

--- src/main/java/kanban_board/controller/HomeController.java
+++ src/main/java/kanban_board/controller/HomeController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home() { return "home"; }
     @GetMapping("/about")
     public String about() { return "about"; }
     @GetMapping("/contact")
     public String contact() { return "contact"; }
+    @GetMapping("/")
+    public String loggedIn() { return "loggedin"; }
 }
