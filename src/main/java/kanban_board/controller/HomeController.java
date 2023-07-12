2670cda3b5eabce097dfd384176c27169c1893f9
Lupe <gpena09@gmail.com>
2023-07-12 07:20:09 -0700

    Starting to add more thymeleaf. Trying to get all pages to show sign out button when user is logged in.

--- src/main/java/kanban_board/controller/HomeController.java
+++ src/main/java/kanban_board/controller/HomeController.java
@@ -1,7 +1,10 @@
 package kanban_board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
@@ -12,5 +15,13 @@ public class HomeController {
     @GetMapping("/contact")
     public String contact() { return "contact"; }
     @GetMapping("/")
-    public String loggedIn() { return "loggedin"; }
+    public String loggedIn(Model model) {
+        // When user is logged in, all pages should have sign out button
+        boolean isLoggedIn = true;
+        model.addAttribute("isLoggedIn", isLoggedIn);
+        return "loggedin"; }
+    @GetMapping("/my_boards")
+    public String myBoards() {
+        // "My Boards" tab appears when user is logged in
+        return "my_boards"; }
 }
