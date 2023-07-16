eceae9065037bf42a018bcc419cbf5aa200ac9ad
Lupe <gpena09@gmail.com>
2023-07-16 10:51:57 -0700

    updated th:href="@{/home}" to "loggedin" in the a tag for "Back to Board List"

--- src/main/resources/static/js/kanban_board.js
+++ src/main/resources/static/js/kanban_board.js
@@ -54,16 +54,19 @@ function createColumnElement(column, columnIndex) {
 
   // Create the edit link
   const editLink = document.createElement("a");
-  editLink.innerHTML = `Edit <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+  editLink.innerHTML = `Edit <svg xmlns="http://www.w3.org/2000/svg" height="1em" fill="dimgray" viewBox="0 0 512 512">
   <path d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z"/></svg>`;
   editLink.href = "#"; // Add your edit link destination here
   editLink.classList.add("menu-link");
+  editLink.classList.add("edit-link");
   menuContainer.appendChild(editLink);
   // Add an event listener for the edit link if needed
 
   // Create the delete link
   const deleteLink = document.createElement("a");
-  deleteLink.innerHTML = 'Delete <i>&#x2715;</i>';
+  deleteLink.innerHTML = `Delete <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                   <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                                 </svg>`;
   deleteLink.href = "#"; // Add your delete link destination here
   deleteLink.classList.add("menu-link");
   menuContainer.appendChild(deleteLink);
