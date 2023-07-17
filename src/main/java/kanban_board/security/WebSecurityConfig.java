58364f2ada3a134d9c7595ff30566948d07c7d95
Lupe <gpena09@gmail.com>
2023-07-17 07:13:47 -0700

    added .antMatchers("/**/*.jpg") to my security config in order to display the images correctly.

--- src/main/java/kanban_board/security/WebSecurityConfig.java
+++ src/main/java/kanban_board/security/WebSecurityConfig.java
@@ -73,6 +73,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web
                 .ignoring()
+                .antMatchers("/**/*.jpg")
                 .antMatchers("/**/*.js")
                 .antMatchers("/static/**")
                 .antMatchers("/**/*.css")
